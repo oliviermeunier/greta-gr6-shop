bed276bab9aab52565f202a1e1d6229b36a9576f
Olivier Meunier <oli.meunier@gmail.com>
2021-11-17 14:57:19 +0100

    Ajout des avis en BDD

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Review;
+use DateTimeImmutable;
 use App\Entity\Product;
+use App\Form\ReviewType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -12,11 +17,26 @@ class ProductController extends AbstractController {
     /**
      * @Route("/product/{slug}", name="product_show")
      */
-    public function show(/*string $slug, ProductRepository $productRepository*/ Product $product)
+    public function show(Product $product, Request $request, EntityManagerInterface $manager)
     {
-        // $product = $productRepository->findOneBy(['slug' => $slug]);
+        $review = new Review(); // On crée un objet Review qui contiendra l'éventuel nouvel avis si l'internaute remplit le formulaire
+        $reviewForm = $this->createForm(ReviewType::class, $review); // On crée le formulaire en lui associant notre objet $review
+        $reviewForm->handleRequest($request); // On transmet les données de la requête au formulaire
 
-        $reviewForm = $this->createForm(ReviewType::class);
+        if ($reviewForm->isSubmitted()) { // Si le formulaire est soumis... 
+
+            // $review contient déjà les 3 champs du formulaire : nickname, content et grade
+            $review->setCreatedAt(new DateTimeImmutable()); // on le complète avec la date du jour
+            $review->setProduct($product); // et le produit associé
+
+            // On l'enrgistre ensuite en base de données
+            $manager->persist($review);
+            $manager->flush();
+
+            // Message flash et redirection
+            $this->addFlash('success', 'Votre avis a bien été pris en compte');
+            return $this->redirectToRoute('product_show', ['slug' => $product->getSlug()]);
+        }
 
         return $this->render('product/show.html.twig', [
             'product' => $product,
--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ReviewType extends AbstractType
 {
