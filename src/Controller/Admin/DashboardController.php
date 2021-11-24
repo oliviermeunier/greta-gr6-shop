693dd18c040eccca5a94c82ce1ed3e030ad0ac8d
Olivier Meunier <oli.meunier@gmail.com>
2021-11-24 09:54:48 +0100

    Fichiers de base pour dashboard admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DashboardController extends AbstractController {
+
+    /**
+     * @Route("/admin", name="admin_dashboard_index")
+     */
+    public function index()
+    {
+        return $this->render('admin/dashboard/index.html.twig');
+    }
+
+}
